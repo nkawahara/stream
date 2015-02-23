c2ee4720553065219bb662bf8b0108c61f1c32fc
sushi <sushi@streamhost.myvps.jp>
2015-02-23 17:42:02 +0900

    2 Function add(Tag parser & save Streaming each tag)

--- test_tweetsstream.rb
+++ test_tweetsstream.rb
@@ -28,29 +28,43 @@ TweetStream.configure do |config|
   config.auth_method        = :oauth
 end
 
-## Redis Connect
-#redis = Redis.new
-
-#stream = TweetStream::client.new
-
-#TweetStream::Client.new.sample do |status|
-#  puts "#{status.text}"
-#end
-
-
+##
+## メインストリーミング
+##
+## 1.キーを与えて指定したDBに全てのデータを保存したメインストリーミングを作る
+## 2.各タグごとのストリーミングを作る関数を呼ぶ
+##
 def keyStream(num,keyword)
   redis_url = "redis://127.0.0.1:6379/#{num}"
   Redis.current = Redis.new(url: redis_url)
-  @list = Redis::List.new('list_#{keyword}', :marshal => true)
+  @list = Redis::List.new("list_#{keyword}", :marshal => true)
   TweetStream::Client.new.track("#{keyword}") do |status|
    if status.user.lang == "ja" && !status.text.index("RT")
       if nil != status.text.match(/\#/)
-        puts "#{status.user.screen_name}:( #{status.id} )  #{status.text}"
-        @list << ["#{status.user.screen_name}","#{status.text}"]
+#        puts "#{status.user.screen_name}:( #{status.id} )  #{status.text}"
+        parser_tag(status)
+        @list << ["#{status.user.screen_name}","#{status.text}"] 
       end
     end
   end
 end
+
+
+## タグパーサ
+def parser_tag(status)
+  ## To Do
+  ## このへんにタグを全て抽出して、
+  ## 再帰的に全てのタグごとに関数を読む記述をする
+end
+
+
+## タグごとにストリームを保存
+def save_stream(tag,status)
+  redis_url = "redis://127.0.0.1:6379/"
+  Redis.current = Redis.new(url: redis_url)
+  @list = Redis::List.new("list_#{tag}", :marshal => true)
+  @list << ["#{status.user.screen_name}","#{status.text}"] 
+end  
 
 keyStream(0,"pic")
 
